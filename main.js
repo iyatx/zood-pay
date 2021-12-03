8c0dfc39a266f469855894a59f77d483cb60686a
AzizbekSavkimov <azizbeksavkimov@mail.ru>
2021-12-03 22:51:01 +0500

    feat: language

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const headerSlider = new Swiper('.header__slider', {
     loop: true,
     effect: "fade",
-    // autoplay: {
-    //     delay: 3000,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
