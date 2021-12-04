7886d4f7c4020b27c9780a945f6c4e21d37ba1ae
AzizbekSavkimov <azizbeksavkimov@mail.ru>
2021-12-04 18:22:21 +0500

    feat: select

--- main.js
+++ main.js
@@ -17,3 +17,11 @@ const thinkSlider = new Swiper('.think__slider', {
     slidesPerView: 6,
     spaceBetween: 30,
 })
+
+
+const languageSelect = document.querySelector('.language__title');
+const dropdownList = document.querySelector('.language__dropdown');
+
+languageSelect.addEventListener('click', (e) => {
+    dropdownList.classList.toggle('dropdown--show')
+})
