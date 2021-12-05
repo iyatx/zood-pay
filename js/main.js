4918a3c13a286288cc3e93b09b72e09cfaeeaed8
AzizbekSavkimov <azizbeksavkimov@mail.ru>
2021-12-05 17:43:56 +0500

    feat: minize app

--- js/main.js
+++ js/main.js
@@ -2,37 +2,6 @@ const state = {
     popupShowClass: 'feedback__popup--show'
 }
 
-
-const headerSlider = new Swiper('.header__slider', {
-    loop: true,
-    effect: "fade",
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-})
-
-
-const thinkSlider = new Swiper('.think__slider', {
-    loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-
-    breakpoints: {
-        600: {
-            slidesPerView: 4
-        },
-        1200: {
-            slidesPerView: 6,
-        },
-    }
-})
-
-
 // Select language variables
 const toggleLanguageDropdown = document.querySelector(".dropdown");
 const activeLanguage = document.querySelector('.text-box');
@@ -43,6 +12,7 @@ const feedbackPopupCloseBtn = document.querySelector('.feedback__popup-cancel');
 const feedbackPopup = document.querySelector('.feedback__popup');
 const feedbackPopupOverlay = document.querySelector('.popup__overlay');
 const feedbackShowPopupBtns = document.querySelectorAll('.feedback-btn');
+
 
 const changeLanguage = (option) => {
     activeLanguage.value = option.textContent
@@ -67,3 +37,33 @@ feedbackShowPopupBtns.forEach((item) => {
     item.addEventListener('click', openFeedbackPopup);
 })
 languageOptions.forEach(item => item.addEventListener('click', () => changeLanguage(item)))
+
+
+// Sliders
+new Swiper('.header__slider', {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+})
+
+new Swiper('.think__slider', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+
+    breakpoints: {
+        600: {
+            slidesPerView: 4
+        },
+        1200: {
+            slidesPerView: 6,
+        },
+    }
+})
